7f0ec8c544e2d2ccde0bd607899dd8dafa7cb124
Ryan Liu <ryanzhliu@gmail.com>
2024-02-14 22:59:46 -0500

    Change layout to hierarchical (with custom node 'level' placement). Add node size visualization based on commandline argument. Add node size increase/decrease on hover enter/exit. Disable edge hover & selection width. Refactor dev.py to support json data with multiple paths.

--- Tree Heirarchy/dev.py
+++ Tree Heirarchy/dev.py
@@ -6,12 +6,14 @@ from pyvis.network import Network
 
 class Node:
     _id_gen = 0
-    def __init__(self, data):
+    def __init__(self, data, size):
         Node._id_gen += 1
         self._uid = Node._id_gen
         self._data = data
         self.children = dict()
-    
+        self._size = size
+        
+
     def get_uid(self):
         return self._uid
 
@@ -47,14 +49,17 @@ class Node:
 
     def get_title(self):
         return f"{self._data['classPath']}.{self._data['methodName']}"
+    
+    def get_size(self):
+        return self._size
 
-def build_call_tree(curr):
-    curr_node = Node(data=methods[curr['id']])
+def build_call_tree(curr, method_map, size_map):
+    curr_node = Node(data=method_map[curr['id']], size=size_map[curr['id']])
     for child in curr['children']:
-        curr_node.add_child(build_call_tree(child))
+        curr_node.add_child(build_call_tree(child, method_map, size_map))
     return curr_node
 
-def build_context_tree(curr):  
+def build_context_tree(curr, method_map, size_map):  
     # TODO: Fix recursion preprocessing bug
     # - occurs when recursion is not just a method calling itself
     #   but rather a method earlier in the exec path
@@ -124,12 +129,12 @@ def build_context_tree(curr):
             prune_call_tree(child_node, visited)
         return curr_node
     
-    ct_root = build_call_tree(curr)
+    ct_root = build_call_tree(curr, method_map, size_map)
     ct_root = prune_call_tree(ct_root, set())
     process_recursions(ct_root, dict())
     return ct_root
 
-def visualize(curr_node, name, max_depth=1000000000, max_edges=1000000000, show=False):
+def visualize(root_list, file_name, max_depth=1000000000, max_edges=1000000000, show=False):
     visited = set()
     edge_count = 0
     def populate(curr_node, depth):
@@ -139,11 +144,12 @@ def visualize(curr_node, name, max_depth=1000000000, max_edges=1000000000, show=
         if depth > max_depth:
             return
         visited.add(curr_node.get_uid())
-        color = 'blue'#'blue' if curr_node.get_id() == root['id'] else 'red'
+        color = 'blue'
         net.add_node(
             n_id=curr_node.get_uid(), 
             label=curr_node.get_label(), 
             color=color,
+            size=curr_node.get_size(),
             title=str(len(curr_node.get_children())))
             
         for child_node in curr_node.get_children():
@@ -157,31 +163,39 @@ def visualize(curr_node, name, max_depth=1000000000, max_edges=1000000000, show=
                     return
     net = Network(height="1000px", width="100%", directed=True, filter_menu=True, select_menu=False)
     # options can be generated from: https://visjs.github.io/vis-network/examples/network/physics/physicsConfiguration.html
-
-    options="""const options = {
-                "physics": {
-                    "repulsion": {
-                    "centralGravity": 0.3,
-                    "springLength": 100,
-                    "springConstant": 0.15,
-                    "nodeDistance": 140,
-                    "damping": 0.15
-                    },
-                    "maxVelocity": 119,
-                    "minVelocity": 0.69,
-                    "solver": "repulsion",
-                    "wind": {
-                    "y": 5
-                    }
-                },
-                "interaction": {"hover": true}
-            }"""
+    options = """
+        const options = {
+            "layout": {
+                "improvedLayout":true,
+                "clusterThreshold": 150,
+                "hierarchical": {
+                    "enabled": true,
+                    "levelSeparation": 150,
+                    "nodeSpacing": 200,
+                    "treeSpacing": 200,
+                    "blockShifting": true,
+                    "edgeMinimization": true,
+                    "parentCentralization": true,
+                    "direction": "UD",        
+                    "sortMethod": "hubsize",  
+                    "shakeTowards": "roots"  
+                }
+            },
+            "edges": {
+                "hoverWidth": 0,
+                "selectionWidth": 0,
+                "width": 1
+            },
+            "interaction": {"hover": true}
+        }
+    """
     net.set_options(options)
     # net.show_buttons(filter_=['physics'])
-    populate(curr_node, 0)
-    net.save_graph(name)
+    for root_node in root_list:
+        populate(root_node, 0)
+    net.save_graph(file_name)
     if show:
-        net.show(name, notebook=False)
+        net.show(file_name, notebook=False)
     
     
 def inject_custom_javascript(base_html, ref_html, out_html, START_TOKEN='CUSTOM START ###', END_TOKEN='CUSTOM END ###'):
@@ -233,33 +247,73 @@ if __name__ == "__main__":
     }
     parser = argparse.ArgumentParser(description='Script for generating customizable call graph visualizations using pyvis.')
     parser.add_argument('--input', '-i', type=str, required=True, help='input (.json) file to process with call graph info.') 
-    parser.add_argument('--out', '-o', type=str, default='vis.html', help='output (.html) file containing call graph visualization.')    
+    parser.add_argument('--out', '-o', type=str, default='', help='output (.html) file containing call graph visualization.')    
     parser.add_argument('--ref', '-r', type=str, default='ref.html', help='reference (.html) file containing custom javascript to inject.')    
     parser.add_argument('--type', '-t', type=str, default='cct', help=f'type of visualization to generate, valid options: {VIS_TYPES}.')
     parser.add_argument('--maxDepth', '-D', type=int, default=1000000000, help=f'max depth in call graph data to visualize.')    
     parser.add_argument('--maxEdges', '-E', type=int, default=1000000000, help=f'max edges in call graph to visualize.')    
+    parser.add_argument('--sizeKey', '-k', type=str, default='methodSize', help=f'default key form metrics list to determine node size in visualization.')    
 
     args = parser.parse_args()
+    if args.type not in VIS_TYPES:
+        print(f'Invalid visualization type: {args.type}')
     out_dir = 'out'
     if os.path.isdir(out_dir) == False:
         os.mkdir(out_dir) 
+    # load json data
     with open(f'{args.input}', 'r') as f:
         data = json.load(f)
-    methods = {m['id']: m for m in data['methods']}
-    paths = data['paths']
-    root = paths[0]
-    if args.type not in VIS_TYPES:
-        print(f'Invalid visualization type: {args.type}')
-    json_name = args.input.split('.json')[0]
+    METHODS = {m['id']: m for m in data['methods']}
+    PATHS = data['paths']
+    # calculate the node size for each method based on args.sizeKey
+    min_size = float('inf')
+    max_size = -float('inf')
+    NODE_SIZE = dict()
+    for k in METHODS:
+        for metric_entry in METHODS[k]['metrics']:
+            if metric_entry['key'] == args.sizeKey:
+                NODE_SIZE[k] = metric_entry['value']
+                min_size = min(min_size, NODE_SIZE[k])
+                max_size = max(max_size, NODE_SIZE[k])
+                break
+
+    # scale node sizes (node with smallest size should have 
+    # the default display size)
+    MAX_DISPLAY_SIZE = 50
+    for k in NODE_SIZE:
+        NODE_SIZE[k] = MAX_DISPLAY_SIZE * (NODE_SIZE[k])/max_size
+
+    # configure file names    
     base_html = f'{out_dir}/{args.type}-base-{args.out}'
     out_html = f'{out_dir}/{args.type}-{args.out}'
-    visualize(VIS_TYPES[args.type](root), base_html, args.maxDepth, args.maxEdges, show=False)
+    if args.out == '':
+        json_name = args.input.split('/')[-1].split('.json')[0]
+        base_html = f'{out_dir}/{args.type}-base-{json_name}.html'
+        out_html = f'{out_dir}/{args.type}-{json_name}.html'
+    print(f'parsing: {args.input}...')
+    root_list = list()
+    # build data strcture in python 
+    for root in PATHS:
+        # build for each starting method
+        root_node = VIS_TYPES[args.type](root, METHODS, NODE_SIZE)
+        root_list.append(root_node)
+    print(f'generating: {base_html}...')
+    # create vis.js output
+    visualize(
+        root_list=root_list, 
+        file_name=base_html, 
+        max_depth=args.maxDepth, 
+        max_edges=args.maxEdges, 
+        show=False)
     # move lib/ directory to out/ folder
     p = os.path.dirname(os.path.abspath(__file__))
     if os.path.isdir(f'{out_dir}/lib'):
         shutil.rmtree(f'{out_dir}/lib')
     os.rename('lib', f'{out_dir}/lib')
+    # inject custom javascript from args.ref
+    print(f'generating: {out_html}...')
     inject_custom_javascript(base_html, args.ref, out_html)
-    print(f'base: {base_html}')
-    print(f'out: {out_html}')
+    print('generated:')
+    print(f'- {base_html}')
+    print(f'- {out_html}')
     
